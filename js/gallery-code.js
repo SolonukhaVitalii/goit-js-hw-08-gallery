de1495dbaef47104470d16f61cf7228af195cc9e
Vitalii <65967741+SolonukhaVitalii@users.noreply.github.com>
2020-10-18 12:46:29 +0300

    work 8 - gallery.js in html

---
+++ js/gallery-code.js
@@ -0,0 +1,22 @@
+import images from './gallery-items.js';
+console.log(images);
+
+/*const galleryContainer = document.querySelector('.gallery');
+const reducedString = images.reduce(acc,{preview, original, description})=>{
+  acc +=
+  '<li class="gallery__item"><a class="gallery__link"href="${original}"><img class="gallery__image"src="${preview}"data-source="${original}"alt="${description}"/></a></li>';
+  return acc});
+  galleryContainer.insertAdjacentHTML('afterbegin', reducedString);
+  */
+
+
+  
+  const galleryRef = document.querySelector('.gallery');
+  
+  images.forEach(el =>
+    galleryRef.insertAdjacentHTML(
+      'beforeend',
+      `<li class="gallery__item"><a class="gallery__link"href="${original}"><img class="gallery__image"src="${preview}"data-source="${original}"alt="${description}"/></a></li>`,
+    ),
+  );
+
