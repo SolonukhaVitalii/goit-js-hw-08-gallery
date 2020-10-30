41e02db0a26512b8c733eae749f6e3c15af84f6a
Vitalii <65967741+SolonukhaVitalii@users.noreply.github.com>
2020-10-30 12:24:21 +0200

    work 8 -final

--- js/gallery-code.js
+++ js/gallery-code.js
@@ -11,25 +11,22 @@ console.log(reducedString);
 
 galleryContainer.insertAdjacentHTML("beforeend", reducedString);
 galleryContainer.addEventListener('click', onClick);
+function onClick(evt) {
+	evt.preventDefault();
+	onOpenModal();
+	openModal.src = evt.target.dataset.source;
+}
 
 const lightBox = document.querySelector('.js-lightbox');
-
 const openModal = document.querySelector('.lightbox__image');
-openModal.addEventListener('click', onOpenModal);
+const closeModal = document.querySelector('[data-action="close-lightbox"]');
+closeModal.addEventListener('click', onCloseModal);
 function onOpenModal() {
-	window.addEventListener('keydown', onEscKeyPress);
 	lightBox.classList.add('is-open');
 }
 
-const closeModal = document.querySelector('[data-action="close-lightbox"]');
-closeModal.addEventListener('click', onCloseModal);
 function onCloseModal() {
-	window.addEventListener('keydown', onEscKeyPress);
-	lightBox.classList.add('is-close');
+	lightBox.classList.remove('is-open');
     lightBox.removeAttribute('src');
     lightBox.removeAttribute("alt");
-}
-
-function onClick(evt) {
-	console.log(evt.target);
 }
